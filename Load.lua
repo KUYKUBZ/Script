ac169008dd20d04f81b5cb8fe0d4f9e0a034ec23
KUYKUBZ <167496381+KUYKUBZ@users.noreply.github.com>
2025-03-23 01:26:42 +0700

    Load.lua

--- Load.lua
+++ Load.lua
@@ -2,6 +2,6 @@ repeat task.wait() until game:IsLoaded()
 local PlaceId = game.PlaceId
 if PlaceId == 91212227996888 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/KUYKUBZ/Script/refs/heads/main/From.lua"))()
-elseif
+else
     game.Players.localPlayer:Kick()
 end
