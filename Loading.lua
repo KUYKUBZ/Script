b186298215037e7c6e9102e1f1186f5787aadc44
KUYKUBZ <167496381+KUYKUBZ@users.noreply.github.com>
2025-03-23 13:15:58 +0700

    Loading.lua

--- Loading.lua
+++ Loading.lua
@@ -3,5 +3,5 @@ local PlaceId = game.PlaceId
 if PlaceId == 91212227996888 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/KUYKUBZ/Script/refs/heads/main/From.lua"))()
 else
-    game.Players.localPlayer:Kick()
+    game.Players.localPlayer:Kick("แมพไม่ตรง")
 end
