b3b3532b21ccb679e9e841d4f456e3610d5a241c
KUYKUBZ <167496381+KUYKUBZ@users.noreply.github.com>
2025-03-23 13:48:57 +0700

    Loading.lua

--- Loading.lua
+++ Loading.lua
@@ -1,7 +1,7 @@
 repeat task.wait() until game:IsLoaded()
 local PlaceId = game.PlaceId
 if PlaceId == 91212227996888 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/KUYKUBZ/Script/refs/heads/main/From.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/KUYKUBZ/Script/refs/heads/main/Fromnew.lua"))()
 else
     game.Players.localPlayer:Kick("แมพไม่ตรง")
 end
